22cd206863a65367e9072592eae271ed3ce4d78d
sibi24sibi <116418076+sibi24sibi@users.noreply.github.com>
2025-03-03 11:21:32 +0530

    removed sixth component  button and added get in touch -3

--- src/pages/Home/Components/GetInTouch.jsx
+++ src/pages/Home/Components/GetInTouch.jsx
@@ -6,7 +6,7 @@ export const GetInTouch = () => {
             <div className="py-16 px-10 rounded-3xl bg-gradient-to-r from-green-400 to-green-600 text-white  ">
 
                 <p className="text-3xl font-bold mb-4">Want a use-case demo for your team?</p>
-                <p className="text-base mb-6 py-4">
+                <p className="text-base mb-6 py-4 w-1/2  ">
                     Reach out to us! We're always eager to bring new data use cases to life, unlocking fresh analysis and AI-driven
                     visual generation on dashboards that empower business teams with instant, insightful visualizations. We also
                     deliver AI-driven insights that enhance data team efficiency with every question they ask.
